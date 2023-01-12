0254cce4e9dcb0b526e2867f08d4b0fd10c64334
Maciej Gajda <gajdam11@gmail.com>
2023-01-12 23:18:50 +0100

    gui added, simple names in browse

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # skrypt obsługujący pdf, konwersja pdf -> docx, wyodrębnianie stron, łączenie pdf'ów
+import os.path
+
 import PyPDF2
 from os import path
+import PySimpleGUI as sg
+
 # TODO metoda konwert -> pdf
 # TODO uwzględnić położenie pliku/ścieżkę
 # TODO metoda wpisująca numery stron do listy
 # TODO dodać instrukcję programu oraz wypisywanie statusu działań
-
-# może zrobię, metoda sprawdzająca czy rozszerzenie jest poprawnę
-# def if_allowed(file_name, action):
-#     pass
 
 
 def check_numbers(numbers_string):
@@ -88,47 +88,64 @@ def create_file_name(name_in):
 # TODO dokończyć action 3
 # TODO zabezpieczyć rozszerzenia plików
 # TODO zabezpieczyć zakres stron
+
 # control
-nums = input("podaj numery: ")
-print(check_numbers(nums))
-action_names = {"1": "merge", "2": "copy pages", "3": "convert to docx"}
-print("Wybierz akcję: ")
-for el in action_names.keys():
-    print(f"{el} - {action_names[el]}")
-action = input('... ')
-# number_list = [0, 2]
+layout_tmp = [
+    [sg.Text('Choose PDF files: ')],
+    [sg.Input(key='pdfs', enable_events=True), sg.FilesBrowse()],
+    [sg.Button('Merge PDFs'), sg.Button('Copy pages')]
+]
 
-# merge
-if action == '1':
-    print(action_names[action])
-    pdf_names = []
-    output_file = ""
-    while True:
-        file = input("Podaj nazwę pliku lub 'q' jeśli chcesz zakończyć: ")
-        file_name = path.basename(file).split('.')[0]
-        if file.lower() == 'q':
-            if not pdf_names:
-                print("Nie podano żadnego pliku!")
-                continue
-            break
-        pdf_names.append(file)
-        output_file += "".join(file_name)
-    output_file += "".join('.pdf')
-    print(output_file)
-    mergePDF(pdf_names, output_file)
+layout_main = [
+    # [sg.Text('What you want to do?')],
+    # [sg.Text('')],
+    [sg.Button('Merge PDFs'), sg.Button('Extract pages'), sg.Button('Convert PDF')]
+]
 
-# copy_pages
-elif action == '2':
-    print(action_names[action])
-    print("")
-    numbers = input("Podaj numery stron. [Możesz podać je po przecinku albo w przedziałach np. 1, 3, 4, 6-9]")
-    # number_list = check_numbers(numbers)
-    number_list = [0, 2]
-    copy_pages(r"C:\Users\gajda\OneDrive\Pulpit\1.pdf", 'plik.pdf', number_list)
-# convert_from
-elif action == '3':
-    print(action_names[action])
-    print("Podaj nazwę pliku, który chcesz przekonwertować")
-    convert_from(r"C:\Users\gajda\OneDrive\Pulpit\ex3.pdf")
 
+window = sg.Window('PDF Manager', layout_main)
+
+while True:
+    merge_layout = [
+        [sg.Text('Choose PDF files:')],
+        [sg.Input(key='pdfs', enable_events=True), sg.FilesBrowse()],
+        [sg.Button('Merge')]
+    ]
+    extract_layout = [
+        [sg.Text('Choose PDF file and pages:')],
+        [sg.Input(key='pdfs', enable_events=True), sg.FilesBrowse()],
+        [sg.Button('Extract')]
+    ]
+    convert_layout = [
+        [sg.Text('Convert PDF:')],
+        [sg.Input(key='pdfs', enable_events=True), sg.FilesBrowse()],
+        [sg.Button('Convert')]
+    ]
+    event, values = window.read()
+
+    if event in (sg.WIN_CLOSED, 'Anuluj'):
+        break
+
+    if event == 'Merge PDFs':
+        merge_window = sg.Window('Merge PDFs', merge_layout)
+        event, values = merge_window.read()
+        if event == 'Merge':
+            # mergePDF()
+            pass
+        elif event == 'pdfs':
+            if values['pdfs']:
+                pdfs = values['pdfs'].split(';')
+                pdfs_names = [os.path.basename(pdf) for pdf in pdfs]
+                # window.FindElement('pdfs').Update(';'.join(pdfs_names))
+                # window[values['pdfs']].Update(';'.join(pdfs_names))
+                input_element = merge_window['pdfs']
+                input_element.Update(';'.join(pdfs_names))
+
+    if event == 'Convert PDF':
+        convert_window = sg.Window('Convert PDF', convert_layout)
+        event, values = convert_window.read()
+
+    if event == 'Extract pages':
+        extract_window = sg.Window('Extract Pages', extract_layout)
+        event, values = extract_window.read()
 
